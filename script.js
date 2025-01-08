5250959e00bbd164fb06ac57489202c9d600f647
lucasfelipe01 <lucas_felipe222@hotmail.com]>
2025-01-07 22:27:12 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Você pode adicionar interações aqui, como animações ou funcionalidades extras.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portfólio carregado com sucesso!");
+  });
+  
+  function toggleMenu() {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('active');
+}
